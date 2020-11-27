2ec2ac9dfb713be15fc43836b77e0540c566fb86
Suhar Prasetyo <suhartyo@Suhars-MacBook-Pro.local>
2020-11-27 16:17:30 +0700

    Summary items

--- app/Http/V1/Controllers/ChecklistItems/SummaryItemsController.php
+++ app/Http/V1/Controllers/ChecklistItems/SummaryItemsController.php
@@ -7,6 +7,8 @@ namespace App\Http\V1\Controllers\ChecklistItems;
 use App\Http\Controllers\Controller;
 use App\Models\Item;
 use Carbon\Carbon;
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class SummaryItemsController extends Controller
@@ -14,20 +16,79 @@ class SummaryItemsController extends Controller
 
     public function execute(Request $request)
     {
-        $items = $this->applyFilter(Item::query(), $request);
-
-
+        return response()->json([
+            'data' => $this->applyFilter($request)
+        ]);
     }
 
-    public function applyFilter($query, $request)
+    public function applyFilter($request)
     {
-        if ($object_domain = $request->query('object_domain')) {
-            $query = $query->where('object_domain', $object_domain);
+        $date = CarbonImmutable::now()->startOfDay();
+        $object_domain = $request->query('object_domain');
+
+        if ($request->query('date')) {
+            $date = CarbonImmutable::createFromTimeString($request->query('date'), $request->query('tz'))->startOfDay();
         }
-        if ($date = $request->query('date')) {
-            $query = $query->where('created_at', '<', Carbon::createFromTimeString($date));
+
+        $result = [
+            'today' => $this->getTotalItems(
+                $object_domain,
+                $date->toMutable(),
+                $date->endOfDay()->toMutable()
+            ),
+            'past_due' => $this->getTotalItems(
+                $object_domain,
+                null,
+                $date->toMutable(),
+                false
+            ),
+            'this_week' => $this->getTotalItems(
+                $object_domain,
+                $date->startOfWeek()->toMutable(),
+                $date->endOfWeek()->toMutable()
+            ),
+            'past_week' => $this->getTotalItems(
+                $object_domain,
+                $date->startOfWeek()->subWeek()->toMutable(),
+                $date->endOfWeek()->subWeek()->toMutable()
+            ),
+            'this_month' => $this->getTotalItems(
+                $object_domain,
+                $date->startOfMonth()->toMutable(),
+                $date->endOfMonth()->toMutable()
+            ),
+            'past_month' => $this->getTotalItems(
+                $object_domain,
+                $date->startOfMonth()->subMonth()->toMutable(),
+                $date->endOfMonth()->subMonth()->toMutable()
+            ),
+        ];
+
+        $result['total'] = array_sum($result);
+
+        return $result;
+    }
+
+    protected function getTotalItems($object_domain, ? Carbon $start_date, ? Carbon $end_date, $is_completed = null)
+    {
+        $query = Item::query();
+
+        if ($object_domain) {
+            $query = $query->whereHas('checklist', function (Builder $builder) use ($object_domain) {
+                $builder->where('object_domain', $object_domain);
+            });
         }
-        return $query;
+        if ($is_completed !== null) {
+            $query = $query->where('is_completed', $is_completed);
+        }
+        if ($start_date) {
+            $query = $query->where('due', '>=', $start_date);
+        }
+        if ($end_date) {
+            $query = $query->where('due', '<=', $end_date);
+        }
+
+        return $query->count();
     }
 
 }
