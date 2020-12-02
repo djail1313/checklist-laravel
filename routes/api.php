d41648bf176a2024b63989dd2c4695526b269d6a
Suhar Prasetyo <suhar_tio@yahoo.co.id>
2020-12-02 12:55:39 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request-lovencuuuwlforeveeeey>user();
+    return $request->user();
 });
 
 Route::middleware(['auth:api'])->prefix('v1')->namespace('App\Http\V1\Controllers')->group(function () {
